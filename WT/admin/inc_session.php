ab86fa47b2bcf7940151b95d158c455cc093cd62
Sajit Maharjan <126838978+Sajit21@users.noreply.github.com>
2024-07-15 18:32:44 +0545

    Update inc_session.php

--- WT/admin/inc_session.php
+++ WT/admin/inc_session.php
@@ -1,4 +1,6 @@
 <?php
+// this sessionn checks whether the time and name is set or not . if not locate to login  page
+
 session_start();
 if(!isset($_SESSION['time']) && !isset($_SESSION['name'])){
     header('location: ../Login.php');
